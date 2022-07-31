9e832084f863158339f739a3a70a0fc6a769b88c
Orion <vitalik_belov_00@mail.ru>
2022-07-31 16:55:17 +0700

    added fragment

--- app/src/main/java/com/orion/musicplayer/MainActivity.java
+++ app/src/main/java/com/orion/musicplayer/MainActivity.java
@@ -14,62 +14,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     private static boolean READ_AUDIO = false;
-    private final AudioReader audioReader = new AudioReader();
-    private final SoundtrackPlayer soundtrackPlayer = new SoundtrackPlayer();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int hasReadMediaPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (hasReadMediaPermission == PackageManager.PERMISSION_GRANTED) {
-            READ_AUDIO = true;
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-        }
-        if (READ_AUDIO) readAudio();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container_view);
 
-
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResult) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResult);
-        if (requestCode == 1 && grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED) {
-            READ_AUDIO = true;
-        }
-
-        if (READ_AUDIO) {
-            readAudio();
-        } else {
-            Toast.makeText(this, "Требуется установить разрешения", Toast.LENGTH_LONG).show();
+        if (fragment == null){
+            fragment = SoundListViewFragment.newInstance();
+            fragmentManager.beginTransaction().
+                    add(R.id.fragment_container_view, fragment).commit();
         }
     }
-
-
-    private void readAudio() {
-        ListView soundtracksList = findViewById(R.id.listSongs);
-        List<String> mediaData = audioReader.getMediaData(this);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mediaData);
-        soundtracksList.setAdapter(adapter);
-
-        soundtracksList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                soundtrackPlayer.playSoundtrack(mediaData, i);
-            }
-        });
-    }
-
-
-
-
 }
